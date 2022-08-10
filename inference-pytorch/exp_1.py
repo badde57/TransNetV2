0e7551724f2e011ece0e2180e68606caced1140f
dengbuqi <617166575@qq.com>
2022-08-10 18:54:54 +0900

    Frame Saving

--- inference-pytorch/exp_1.py
+++ inference-pytorch/exp_1.py
@@ -8,7 +8,7 @@ import os
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-video_path = "/home/deng/data/video_info_extraction/code/utils/test.mp4"
+video_path = "./test.mp4"
 file_name = video_path.split('/')[-1].split('.')[0]
 
 model = TransNetV2(device=device)
@@ -33,6 +33,11 @@ with torch.no_grad():
             os.makedirs(f'{file_name}')
         for idx, frame in enumerate(iio.imiter(video_path)):
             if idx in scenes:
-                iio.imwrite(f"{file_name}/frame{idx:03d}.jpg", frame)
+                iio.imwrite(f"{file_name}/frame{idx:03d}.jpg", frame) # Takes a long time!
     print(time.time() - start,'Image saved!')
     # model.predictions_to_scenes(single_frame_predictions)
+
+# [TransNetV2] Extracting frames from ./test.mp4
+# [TransNetV2] Processing video frames 24965/24965
+# 33.428701400756836 detection finished!
+# 73.72723340988159 Image saved!
